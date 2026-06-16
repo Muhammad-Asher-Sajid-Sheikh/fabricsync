341dd813eb496d21851054eaca5a5d008d88b193
Abdul Wasay <irfansoomro370@gmail.com>
2026-06-16 21:05:32 +0500

    resolved docker connection issue by setting up network between postgres and backend now backend communicates with postgres and at time of intialization the prisma schema is loaded into the databse

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,6 +6,10 @@ const PORT = 3000
 
 app.use(clerkMiddleware())
 
+app.get('/health', (req, res)=> {
+  res.status(200).json({ status: 'OK' })
+})
+
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`)
